80c842d1f1e7c4fc296383bff4cd9858438ef65a
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-03 16:02:52 -0500

    unit test when opening and closing key correctly {}

--- stack.test.js
+++ stack.test.js
@@ -4,8 +4,15 @@ const stringtoValide =`const addOne = value =>{
 return value +1;
 }};` 
 
+const stringvalidadecorrect=`const addOne = value =>{
+    return value +1;
+    };` 
 
 
 test('in whatever case }} should return false'+stringtoValide, () => {
 expect(doBracketsBalance(stringtoValide)).toBeFalsy();
 });
+
+test('in whatever case } should return true'+stringtoValide, () => {
+    expect(doBracketsBalance(stringvalidadecorrect)).toBeTruthy();
+    });
