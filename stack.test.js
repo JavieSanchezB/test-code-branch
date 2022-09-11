6276c5ba6c478fbe73e93b3b87f0441ff6dc6f7a
horusjavier <kornjaviersanchezb@gmail.com>
2022-09-10 21:24:38 -0500

    in whatever case ({[)]} should return true

--- stack.test.js
+++ stack.test.js
@@ -8,11 +8,24 @@ const stringvalidadecorrect=`const addOne = value =>{
     return value +1;
     };` 
 
+    
+const stringvalidadecorrectkeysfull=`{[()]}` 
+const stringvalidadecorrectkeysfullincorrect=`({[)]}` 
+
 
 test('in whatever case }} should return false'+stringtoValide, () => {
 expect(doBracketsBalance(stringtoValide)).toBeFalsy();
 });
 
-test('in whatever case } should return true'+stringtoValide, () => {
+test('in whatever case } should return true'+stringvalidadecorrect, () => {
     expect(doBracketsBalance(stringvalidadecorrect)).toBeTruthy();
     });
+
+    test('in whatever case {[()]} should return true'+stringvalidadecorrectkeysfull, () => {
+        expect(doBracketsBalance(stringvalidadecorrectkeysfull)).toBeTruthy();
+        });
+
+        test('in whatever case ({[)]} should return true'+stringvalidadecorrectkeysfullincorrect, () => {
+            expect(doBracketsBalance(stringvalidadecorrectkeysfullincorrect)).toBeFalsy();
+            });
+    
